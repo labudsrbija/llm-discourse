d5709114162070aea85bd9a7cb58d4d6953a98c3
sfilipoviccc <stefan.filipovic@concentrical.com>
2024-03-26 02:26:44 +0100

    move to document ready

--- javascripts/discourse/components/below-site-header/below-site-header.js
+++ javascripts/discourse/components/below-site-header/below-site-header.js
@@ -5,34 +5,39 @@ export function testonijaTest() {
 }
 
 
-document.getElementById('ajaxButton').addEventListener('click', function() {
-  // Data to be sent
-  const dataToSend = {
-    key1: 'value1',
-    key2: 'value2'
-  };
+$(document).ready(function() {
+  console.log('READY?!?!?!');
 
-  console.log('test ajax');
+  document.getElementById('ajaxButton').addEventListener('click', function() {
+    // Data to be sent
+    const dataToSend = {
+      key1: 'value1',
+      key2: 'value2'
+    };
 
-  fetch('https://webhook.site/458b13f4-f481-4cbd-b8c8-a1312260aa49', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(dataToSend)
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json(); // assuming the response is JSON
-  })
-  .then(data => {
-    console.log('Response from server:', data);
-    // Handle the response data here
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-    // Handle errors here
+    console.log('test ajax');
+
+    fetch('https://webhook.site/458b13f4-f481-4cbd-b8c8-a1312260aa49', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataToSend)
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // assuming the response is JSON
+    })
+    .then(data => {
+      console.log('Response from server:', data);
+      // Handle the response data here
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+      // Handle errors here
+    });
   });
 });
+
