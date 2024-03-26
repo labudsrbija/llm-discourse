0805b67ea1d4d0fb99a213c4ffc6641df5c55c8f
sfilipoviccc <stefan.filipovic@concentrical.com>
2024-03-26 02:17:32 +0100

    move js ajax code

--- javascripts/discourse/components/below-site-header/below-site-header.js
+++ javascripts/discourse/components/below-site-header/below-site-header.js
@@ -3,3 +3,36 @@ export function testonijaTest() {
     let test2 = document.querySelector('.testonijaTest');
     console.log('testonija222>>> ', test2);
 }
+
+
+document.getElementById('ajaxButton').addEventListener('click', function() {
+  // Data to be sent
+  const dataToSend = {
+    key1: 'value1',
+    key2: 'value2'
+  };
+
+  console.log('test ajax');
+
+  fetch('https://webhook.site/458b13f4-f481-4cbd-b8c8-a1312260aa49', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dataToSend)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // assuming the response is JSON
+  })
+  .then(data => {
+    console.log('Response from server:', data);
+    // Handle the response data here
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+    // Handle errors here
+  });
+});
