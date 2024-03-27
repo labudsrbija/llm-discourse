48e2c84396f4aa5c23f14e0a9d314ec859917975
sfilipoviccc <stefan.filipovic@concentrical.com>
2024-03-28 00:35:13 +0100

    try string.raw

--- javascripts/discourse/components/below-site-header/below-site-header.js
+++ javascripts/discourse/components/below-site-header/below-site-header.js
@@ -47,8 +47,10 @@ $(document).ready(function() {
     })
     .then(data => {
       console.log('Response from server:', data);
-      const formattedBody = data.body.replace(/\n/g, '<br>');
+      // const formattedBody = data.body.replace(/\n/g, '<br>');
+      const formattedBody = String.raw`${data.body}`;
       document.getElementById('responseTextArea').value = formattedBody;
+      // document.getElementById('responseTextArea').value = formattedBody;
       // Handle the response data here
     })
     .catch(error => {
